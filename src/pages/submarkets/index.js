3d6371b1ca11841c79df05a551bb78a0f0a5e8f3
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-08-15 11:01:22 -0400

    submarkets page edits

--- src/pages/submarkets/index.js
+++ src/pages/submarkets/index.js
@@ -9,37 +9,22 @@ export default function Submarkets({ data }) {
   edges = useRef(Array.from(edges, ({ node: { frontmatter } }) => frontmatter));
 
   return (
-    <div className="max-w-5xl mx-auto">
-      <div className="w-full flex justify-center text-5xl py-12">
-        What is a submarket?
-      </div>
-      <div>
-        <h3 className="text-2xl font-bold">Submarkets</h3>
-        {edges.current.map((submarket) => (
-          <div className="flex py-12">
-            <div className="w-full h-72 bg-gray-300 flex items-center justify-center mr-10">
-              map
-            </div>
-            <div>
-              <Link
-                className="text-2xl underline font-bold"
-                to={submarket.slug}
-              >
-                {submarket.title}
-              </Link>
-              <div className="text-xl">{submarket.description}</div>
-              <div className="text-xl">
-                <h3>Highlights</h3>
-                <ul className="list-disc ml-8">
-                  {submarket.highlights.map((highlight) => (
-                    <li>{highlight}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
+    <div className="px-12">
+      <h3 className="text-2xl font-bold">Submarkets Overview</h3>
+      {edges.current.map((submarket) => (
+        <div className="flex py-2 items-start" key={submarket.title}>
+          <div
+            className="p-2.5 mr-2 mt-1"
+            style={{ backgroundColor: submarket.hex }}
+          />
+          <div>
+            <Link className="text-xl font-bold" to={submarket.slug}>
+              {submarket.title}
+            </Link>
+            <div>{submarket.description}</div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
@@ -54,6 +39,7 @@ export const pageQuery = graphql`
             title
             highlights
             slug
+            hex
           }
         }
       }
