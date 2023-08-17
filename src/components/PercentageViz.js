c7db201fb53824cb475321de777431ba78f8eb1e
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-08-17 11:57:39 -0400

    municipality viz start

---
+++ src/components/PercentageViz.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PercentageViz = (values) => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
+
+export default PercentageViz;
--- src/pages/[county]/[municipality]/index.js
+++ src/pages/[county]/[municipality]/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { submarketHex } from "../../../utils/index";
 
 const MunicipalityPage = (props) => {
   const { county, municipality } = props.params;
+  const res = {
+    help: "https://catalog.dvrpc.org/api/3/action/help_show?name=datastore_search_sql",
+    success: true,
+    result: {
+      sql: "SELECT * from \"66807b47-0d95-4671-913b-0bf8e61d878e\" WHERE geoid ='3400503370'",
+      records: [
+        {
+          _id: 1,
+          _full_text: "#full str of json",
+          geoid: "3400503370",
+          name: "Bass River Township",
+          county: "Burlington",
+          one: 0,
+          two: 0.12,
+          three: 0,
+          four: 0.27,
+          five: 0,
+          six: 0,
+          seven: 0.61,
+          eight: 0,
+        },
+      ],
+      fields: [{ "array of fields from table": "" }],
+    },
+  };
 
   return (
     <div>
