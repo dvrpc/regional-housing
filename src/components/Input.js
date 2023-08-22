3074bee6cbd259edb9db770c2b05e9f1de6b4be8
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-08-22 09:27:32 -0400

    home page mockups

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ placeholder }) => {
   return (
     <input
-      class="text-sm font-normal appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 focus:outline-none focus:bg-white focus:border-blue-500"
+      class="font-normal appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 focus:outline-none focus:bg-white focus:border-blue-500"
       type="text"
       placeholder={placeholder ? placeholder : ""}
     ></input>
