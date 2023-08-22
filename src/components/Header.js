4125be6e5f5e60a6f421114934f3fa5c38b02808
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-08-22 09:37:09 -0400

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,21 @@ import React from "react";
 import { Link } from "gatsby";
 import Input from "./Input";
 import DVRPCMini from "../images/dvrpc-mini.svg";
+import Logo from "../images/logo.png";
 
 const Header = () => {
   return (
     <header className="bg-[#f05a22] text-white flex drop-shadow-lg px-36 h-[15vh] z-[999]">
       <div className="flex items-center">
         <img src={DVRPCMini} alt="dvrpc logo" className="md:h-16 opacity-50" />
+        <img
+          src={Logo}
+          alt="housing submarkets logo"
+          className="md:h-28 mx-20"
+        />
       </div>
-      <div className="ml-auto p-4 md:w-2/5">
-        <ul className="flex divide-x justify-end [&>*]:px-6 font-bold">
+      <div className="ml-auto p-4 md:w-2/5 flex flex-col justify-center">
+        <ul className="flex divide-x justify-end [&>*]:px-2.5 font-bold">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -21,7 +27,7 @@ const Header = () => {
             <Link to="/about">About</Link>
           </li>
         </ul>
-        <div className="flex pr-6 space-x-4 my-4 items-center">
+        <div className="flex pr-2 space-x-4 my-4 items-center">
           <span className="font-bold whitespace-nowrap">FIND A COMMUNITY:</span>
           <Input placeholder="Search by county or municipality" />
         </div>
