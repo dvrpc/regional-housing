28b072860a5385420e32c363fe9966bb30e83743
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-08-25 16:57:33 -0400

    allow for state variables inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Input = ({ placeholder }) => {
+const Input = ({ placeholder, value = null, onChange = null }) => {
   return (
     <input
-      class="font-normal appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 focus:outline-none focus:bg-white focus:border-blue-500"
+      class="text-black font-normal appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 focus:outline-none focus:bg-white focus:border-blue-500"
       type="text"
       placeholder={placeholder ? placeholder : ""}
+      value={value}
+      onChange={onChange}
     ></input>
   );
 };
