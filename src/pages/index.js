4cf32f0d7a59e1b79caaab813b4a643f5b02b1b6
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-10-19 10:41:23 -0400

    updates from cs

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ const IndexPage = () => {
     <div className="min-h-screen">
       <div className="w-full">
         <div
-          className="p-4 md:px-36 md:py-6 text-center bg-left md:mt-0"
+          className="p-4 md:px-36 md:py-6 text-center bg-left md:mt-0 bg-cover"
           style={{ backgroundImage: `url(${Hero})` }}
         />
-        <div className="p-4 md:px-36 md:py-12 text-center border-b-4 border-[#A198D1] bg-white">
+        <div className="p-4 md:px-36 md:py-12 text-center border-b-[2px] border-[#A198D1] bg-white">
           <h1 className="text-[2rem] md:text-5xl font-bold leading-snug text-[#6C9CCD]">
             A Regional Approach to Housing Affordability
           </h1>
@@ -31,61 +31,61 @@ const IndexPage = () => {
           <p className="flex flex-col items-center">
             <div className="flex flex-col px-2.5 md:w-full">
               <div className="md:flex md:items-center md:pb-14">
-                <span className="font-bold my-auto w-full text-center tracking-[2px] md:mr-2 inline-block w-1/3 whitespace-nowrap text-[#6C9CCD] text-2xl py-4 md:w-1/3 md:text-left">
+                <span className="font-bold my-auto w-full text-center tracking-[2px] md:mr-6 inline-block w-1/3 whitespace-nowrap text-[#6C9CCD] text-2xl py-4 md:w-1/3 md:text-right">
                   FIND A COMMUNITY:
                 </span>
                 <Search />
               </div>
               <div className="md:flex md:items-center md:w-full md:pb-4">
-                <span className="font-bold my-auto w-full text-center tracking-[2px] md:mr-2 inline-block w-1/3 whitespace-nowrap text-[#6C9CCD] text-2xl py-4 md:w-1/3 md:text-left">
+                <span className="font-bold my-auto w-full text-center tracking-[2px] md:mr-6 inline-block w-1/3 whitespace-nowrap text-[#6C9CCD] text-2xl py-4 md:w-1/3 md:text-right">
                   BROWSE BY COUNTY:
                 </span>
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:w-full">
                   <Link
                     to="bucks"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold "
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Bucks
                   </Link>
                   <Link
                     to="burlington"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Burlington
                   </Link>
                   <Link
                     to="camden"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Camden
                   </Link>
                   <Link
                     to="chester"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Chester
                   </Link>
                   <Link
                     to="delaware"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Delaware
                   </Link>
                   <Link
                     to="gloucester"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Gloucester
                   </Link>
                   <Link
                     to="montgomery"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Montgomery
                   </Link>
                   <Link
                     to="philadelphia"
-                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold"
+                    className="bg-[#f05a22] text-white p-3 rounded-lg font-bold hover:bg-[#db5421]"
                   >
                     Philadelphia
                   </Link>
@@ -94,7 +94,7 @@ const IndexPage = () => {
             </div>
           </p>
         </div>
-        <div className="p-4 md:px-36 md:py-20 border-b-4 border-[#A198D1] bg-white">
+        <div className="p-4 md:px-36 md:py-20 border-b-[2px] border-[#A198D1] bg-white">
           <div className="grid md:grid-cols-2 text-[#f05a22] md:space-x-8 space-y-4 md:space-y-0">
             <p>
               <strong className="block text-xl">What is a submarket?</strong>
@@ -135,24 +135,36 @@ const IndexPage = () => {
             <span className="md:w-1/2">
               <textarea
                 placeholder="Comments and/or Questions (optional)"
-                className="font-normal appearance-none border-2 border-gray-200 rounded-lg w-full h-full py-2 px-4 md:mx-4 focus:outline-none focus:bg-white focus:border-blue-500 my-4 md:my-0"
+                className="font-normal appearance-none border-[1px] border-[#f05a22] rounded-lg w-full h-full py-2 px-4 md:mx-4 focus:outline-none focus:bg-white focus:border-blue-500 my-4 md:my-0"
               />
             </span>
           </form>
         </div>
         <footer>
-          <div className="bg-[#3D78B4] flex flex-col-reverse md:flex-row p-4 md:px-36 md:py-14 justify-start divide-x space-x-4 justify-center">
-            <img
-              src={DVRPC}
-              alt="dvrpc logo"
-              className="h-24 mt-1 object-scale-down"
-            />
-            <p className="text-white md:ml-24 pl-4">
+          <div className="bg-[#6C9CCD] flex flex-col-reverse md:flex-row p-4 md:px-36 md:py-14 justify-start divide-x justify-center w-full">
+            <span className="w-1/2">
+              <a href="https://www.dvrpc.org">
+                <img
+                  src={DVRPC}
+                  alt="dvrpc logo"
+                  className="h-24 mt-1 object-scale-down pr-10"
+                  align="right"
+                />
+              </a>
+            </span>
+            <p className="text-white pl-10 text-left w-1/2">
               190 N Independence Mall West
               <br /> 8th Floor
               <br /> Philadelphia PA 19106-1520
               <br /> 215.592.1800
-              <br /> www.dvrpc.org
+              <br />{" "}
+              <a
+                href="https://www.dvrpc.org"
+                target="_blank"
+                className="underline"
+              >
+                www.dvrpc.org
+              </a>
               <div className="flex">
                 <span className="font-bold">Connect With Us!</span>
                 <span className="flex [&>*]:px-2 divide-x items-center">
@@ -160,6 +172,7 @@ const IndexPage = () => {
                     key="facebook"
                     href="https://www.facebook.com/DVRPC"
                     label="Facebook"
+                    target="_blank"
                   >
                     <img
                       src={Facebook}
@@ -171,6 +184,7 @@ const IndexPage = () => {
                     key="twitter"
                     href="https://www.twitter.com/DVRPC"
                     label="Twitter"
+                    target="_blank"
                   >
                     <img src={Twitter} alt="twitter logo" className="h-4 w-4" />
                   </a>
@@ -178,17 +192,20 @@ const IndexPage = () => {
                     key="instagram"
                     href="https://www.instagram.com/dvrpc/"
                     label="Instagram"
+                    target="_blank"
                   >
                     <img
                       src={Instagram}
                       alt="instagram logo"
                       className="h-4 w-4"
+                      target="_blank"
                     />
                   </a>
                   <a
                     key="linkedin"
                     href="http://www.linkedin.com/company/delaware-valley-regional-planning-commission"
                     label="LinkedIn"
+                    target="_blank"
                   >
                     <img
                       src={Linkedin}
@@ -200,6 +217,7 @@ const IndexPage = () => {
                     key="youtube"
                     href="https://www.youtube.com/channel/UCEU8UI5_iGkVypHP93b5jLA"
                     label="YouTube"
+                    target="_blank"
                   >
                     <img src={Youtube} alt="youtube logo" className="h-4 w-4" />
                   </a>
@@ -209,10 +227,28 @@ const IndexPage = () => {
           </div>
           <div className="text-[#f05a22] w-full text-center [&>*]:py-2 p-12">
             <p className="font-bold">
-              Learn more about housing initiatives at www.dvrpc.org/housing
+              Learn more about housing initiatives at{" "}
+              <a
+                className="underline"
+                href="https://www.dvrpc.org/housing"
+                target="_blank"
+              >
+                www.dvrpc.org/housing
+              </a>
             </p>
             <p className="italic divide-y">
-              Product Number: TEM458 | DVRPC Policies
+              Product Number:{" "}
+              <a href="/" className="underline">
+                TEM458
+              </a>{" "}
+              |{" "}
+              <a
+                className="underline"
+                href="https://www.dvrpc.org/policies"
+                target="_blank"
+              >
+                DVRPC Policies
+              </a>
             </p>
             <p>
               <span className="font-bold">
