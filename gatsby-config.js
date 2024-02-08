51f26935242987b80154c2d2e4074ba80efbf431
hachadorian <hachadorian98@gmail.com>
2024-02-08 12:18:23 -0500

    icon, head tag, analytics
    metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `regional-housing`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Regional Housing Application`,
+    description:
+      "A housing submarket is composed of neighborhoods with similar housing stock and market characteristics. These characteristics play a large role in determining who can move to or remain in suitable housing in that neighborhood. Staff conducted a regional housing submarket analysis that used various data points and a clustering algorithm to identify eight submarkets across Greater Philadelphia. The results of this analysis can be used to identify neighborhoods that share common needs and challenges regardless of geographic location and promote regional dialogue about the policies and strategies that local governments can use to improve the affordability of housing in their communities.",
+    author: "Delaware Valley Regional Planning Commission (DVRPC)",
+    siteUrl: "https://www.dvrpc.org/housing/submarkets",
   },
   pathPrefix: `/housing/submarkets`,
   plugins: [
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-postcss",
     "gatsby-plugin-mdx",
     {
@@ -20,5 +24,20 @@ module.exports = {
     `gatsby-plugin-layout`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "DVRPC Regional Housing Application",
+        short_name: "Regional Housing",
+        start_url: "/",
+        icon: "src/images/bug-highres.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-9825778-1",
+      },
+    },
   ],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,11 +2,16 @@ import React, { useContext, useEffect } from "react";
 import DVRPCMap from "../components/DVRPCMap";
 import Header from "../components/Header";
 import AppContext from "../utils/AppContext";
+import { Helmet } from "react-helmet";
+import { titleCase } from "../utils";
 
 const Layout = (props) => {
   const { county, municipality } = props;
   const { setSubmarketFilter } = useContext(AppContext);
   const isHome = props.pageContext.layout === "home";
+  let title = "";
+  if (!municipality && !county) title = null;
+  else title = !municipality ? titleCase(county) : titleCase(municipality);
 
   useEffect(() => {
     if (isHome || props.path === "/submarkets/") setSubmarketFilter("");
@@ -18,25 +23,33 @@ const Layout = (props) => {
   }, [props.path]);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <link
-        href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css"
-        rel="stylesheet"
-      />
-      <Header isHome={isHome} />
-      {props.pageContext.layout === "home" ||
-      props.pageContext.layout === "about" ? (
-        props.children
-      ) : (
-        <div style={{ height: "85vh" }}>
-          <div style={{ width: "100%" }}>
-            <DVRPCMap params={{ county, municipality }}>
-              {props.children}
-            </DVRPCMap>
+    <>
+      <Helmet>
+        <html lang="en"></html>
+        <title>
+          {!title ? "Regional Housing" : "Regional Housing - " + title}
+        </title>
+      </Helmet>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <link
+          href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+        <Header isHome={isHome} />
+        {props.pageContext.layout === "home" ||
+        props.pageContext.layout === "about" ? (
+          props.children
+        ) : (
+          <div style={{ height: "85vh" }}>
+            <div style={{ width: "100%" }}>
+              <DVRPCMap params={{ county, municipality }}>
+                {props.children}
+              </DVRPCMap>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
